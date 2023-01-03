5d95c7aebed6054a8bb4393041e61066f95ca7cf
Sneaky Koder <sneaky.koder@gmail.com>
2023-01-03 23:39:17 +0000

    Styling updates

--- src/app.jsx
+++ src/app.jsx
@@ -1,4 +1,8 @@
-import { useState } from "preact/hooks";
+import { Container } from "@mui/system";
+import { useState, useMemo } from "preact/hooks";
+import useMediaQuery from '@mui/material/useMediaQuery';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
 import "./app.css";
 import HeightAndWeightInput from "./components/height-and-weight";
 import Results from "./components/results";
@@ -12,6 +16,17 @@ export function App() {
   const [bmi, set_bmi] = useState(NaN);
   const [height, set_height] = useState(NaN);
 
+  const prefersDarkMode = useMediaQuery('(prefers-color-scheme: dark)');
+  const theme = useMemo(
+    () => createTheme(
+      {
+        palette: {
+            mode: prefersDarkMode ? 'dark' : 'light',
+          },
+        }),
+      [prefersDarkMode],
+    );
+
   function calculateHandler(weight, height) {
     const bmi = calculateBMI(weight, height);
     set_bmi(bmi);
@@ -19,11 +34,14 @@ export function App() {
   }
 
   return (
-    <div>
-      <h1>Body Mass Index</h1>
-      <HeightAndWeightInput onCalculate={calculateHandler} />
-      <hr />
-      <Results bmi={bmi} height={height} />
-    </div>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Container fluid>
+        <h1>Body Mass Index</h1>
+        <HeightAndWeightInput onCalculate={calculateHandler} />
+        <hr />
+        <Results bmi={bmi} height={height} />
+      </Container>
+    </ThemeProvider>
   );
 }
--- src/components/height-and-weight.jsx
+++ src/components/height-and-weight.jsx
@@ -1,48 +1,55 @@
-import { Fragment } from "preact";
-import { useRef, useState } from 'preact/hooks';
-import Results from "./results";
+import { Button, Grid, TextField, InputAdornment } from "@mui/material";
+import { Box } from "@mui/system";
+import { useRef } from 'preact/hooks';
 
 function HeightAndWeightInput({ onCalculate }) {
     const height_ref = useRef(null);
     const weight_ref = useRef(null);
 
+    const itemStyle = { display: 'flex', justifyContent: 'center', alignContent: 'center', justifyItems: 'center', alignItems: 'center' };
+
     return (
-        <div>
-            <section>
-                <span>Height</span>
-                <input
-                    type="number"
-                    min={1}
-                    max={2.5}
-                    step={0.1}
-                    placeholder="Height"
-                    aria-label="Height"
-                    ref={height_ref}
-                />
-                <span>mts.</span>
-            </section>
+        <Grid container spacing={1}>
+            <Grid item xs={0}  sm={3} md={4} /> {/* spacing */}
+            <Grid item xs={12} sm={6} md={4} >
+                <Box sx={itemStyle} >
+                    <TextField label="Height"
+                        variant="outlined"
+                        type="number"
+                        min={1}
+                        max={2.5}
+                        step={0.1} 
+                        endAdornment={<InputAdornment position="end">kg</InputAdornment>}
+                        inputRef={height_ref} />
+                </Box>
+            </Grid>
+            <Grid item xs={0}  sm={3} md={4} /> {/* spacing */}
 
-            <section>
-                <span>Weight</span>
-                <input
-                    type="number"
-                    min={1}
-                    max={500}
-                    step={0.1}
-                    placeholder="Height"
-                    aria-label="Height"
-                    ref={weight_ref}
-                />
-                <span>kgs.</span>
-            </section>
+            <Grid item xs={0}  sm={3} md={4} /> {/* spacing */}
+            <Grid item xs={12} sm={6} md={4} >
+                <Box sx={itemStyle} >
+                    <TextField label="Weight"
+                        variant="outlined"
+                        type="number"
+                        min={1}
+                        max={500}
+                        step={0.1}
+                        inputRef={weight_ref} />
+                </Box>
+            </Grid>
+            <Grid item xs={0}  sm={3} md={4} /> {/* spacing */}
 
-            <section>
-                <button
-                    type="button"
-                    onClick={() => { onCalculate(weight_ref.current.value, height_ref.current.value); }}
-                >Calculate</button>
-            </section>
-        </div>
+            <Grid item xs={0}  sm={3} md={4} /> {/* spacing */}
+            <Grid item xs={12} sm={6} md={4}> 
+                <Box sx={itemStyle}>
+                    <Button variant="outlined" color="primary" 
+                            onClick={() => onCalculate(weight_ref.current.value, height_ref.current.value)}>
+                        Calculate
+                    </Button>
+                </Box>
+            </Grid>
+            <Grid item xs={0}  sm={3} md={4} /> {/* spacing */}
+        </Grid>
     );
 }
 
